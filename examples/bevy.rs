67c1abfb63773ff0a26dbfb3ad7affe75fb29617
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-04-06 10:48:02 -0600

    feat: Pan+orbit camera for Bevy example

--- examples/bevy.rs
+++ examples/bevy.rs
@@ -8,11 +8,13 @@ use bevy::{
         render_resource::PrimitiveTopology,
     },
 };
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 use hexglobe::projection::globe::{ExactFace, ExactGlobe};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
         .run();
 }
@@ -41,6 +43,7 @@ fn setup(
     // camera
     commands.spawn((
         Camera3d::default(),
+        PanOrbitCamera::default(),
         Transform::from_xyz(-1.5, 2.5, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
