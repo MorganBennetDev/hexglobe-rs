319a34971f7c2604b77b4141fcf003951dd53e10
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-04-10 17:10:56 -0600

    fix: Projection benchmark was not measuring the most expensive part of the process.

--- benches/projection.rs
+++ benches/projection.rs
@@ -7,5 +7,8 @@ fn main() {
 
 #[divan::bench(consts = [1, 2, 4, 8, 16, 32, 64])]
 fn projection<const N: u32>() {
-    ExactGlobe::<N>::new();
+    let g = ExactGlobe::<N>::new();
+    
+    g.mesh_vertices(None);
+    g.mesh_triangles();
 }
