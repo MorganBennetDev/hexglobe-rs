82010401bff78f668ccbfd857fd5e357013874a9
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-04-21 10:51:34 -0600

    chore: Adjacency calculation benchmarking.

---
+++ benches/adjacency.rs
@@ -0,0 +1,13 @@
+use hexglobe::projection::globe::ExactGlobe;
+
+fn main() {
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench(consts = [1, 2, 4, 8, 16, 32, 64])]
+fn adjacency<const N: u32>() {
+    let g = ExactGlobe::<N>::new();
+    
+    g.adjacency();
+}
